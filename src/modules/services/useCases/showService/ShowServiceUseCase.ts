0b8c92b0ab3585ff3d8056bb3eb1c21b1fa7e098
Rodrigo César <rodrigocnn@gmail.com>
2024-02-02 19:35:10 -0300

    add strict true and return showService

--- src/modules/services/useCases/showService/ShowServiceUseCase.ts
+++ src/modules/services/useCases/showService/ShowServiceUseCase.ts
@@ -1,21 +1,22 @@
-import { inject, injectable } from "tsyringe"
+import { inject, injectable } from 'tsyringe';
 
-import { IServicesRepository } from "../../repositories/IServicesRepository"
+import { IServicesRepository } from '../../repositories/IServicesRepository';
 
 @injectable()
 export class ShowServiceUseCase {
   constructor(
-    @inject("ServicesRepositoryPrisma")
-    private servicesRepository: IServicesRepository,
+    @inject('ServicesRepositoryPrisma')
+    private servicesRepository: IServicesRepository
   ) {}
 
   async execute(id: string) {
     if (this.servicesRepository.exists) {
-      const roleExists = await this.servicesRepository.show(id)
+      const roleExists = await this.servicesRepository.show(id);
       if (!roleExists) {
-        throw new Error(`Role with id ${id} not found`)
+        throw new Error(`Role with id ${id} not found`);
       }
-      return roleExists
+      return roleExists;
     }
+    return null;
   }
 }
