1dce7630fd46419d264782b821b924aa342a4de0
Rodrigo César <rodrigocnn@gmail.com>
2024-02-02 19:39:36 -0300

    return null to professional

--- src/modules/professionals/useCases/showProfessional/ShowProfessionalUseCase.ts
+++ src/modules/professionals/useCases/showProfessional/ShowProfessionalUseCase.ts
@@ -1,21 +1,22 @@
-import { inject, injectable } from "tsyringe"
+import { inject, injectable } from 'tsyringe';
 
-import { IProfessionalsRepository } from "../../repositories/IProfessionalsRepository"
+import { IProfessionalsRepository } from '../../repositories/IProfessionalsRepository';
 
 @injectable()
 export class ShowProfessionalUseCase {
   constructor(
-    @inject("ProfessionalsRepositoryPrisma")
-    private professionalsRepository: IProfessionalsRepository,
+    @inject('ProfessionalsRepositoryPrisma')
+    private professionalsRepository: IProfessionalsRepository
   ) {}
 
   async execute(id: string) {
     if (this.professionalsRepository.exists) {
-      const roleExists = await this.professionalsRepository.show(id)
+      const roleExists = await this.professionalsRepository.show(id);
       if (!roleExists) {
-        throw new Error(`Role with id ${id} not found`)
+        throw new Error(`Role with id ${id} not found`);
       }
-      return roleExists
+      return roleExists;
     }
+    return null;
   }
 }
